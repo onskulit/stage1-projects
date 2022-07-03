cf7707c09df85d21b091b41af996f0d48f733e30
onskulit <dislaty@mail.com>
2022-07-03 15:29:31 +0300

    feat: migrate appLoader.js to .ts

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '145a3d7de4f242a6ba17b7bc2e18f6f2', // получите свой ключ https://newsapi.org/
+            apiKey: '145a3d7de4f242a6ba17b7bc2e18f6f2',
         });
     }
 }
