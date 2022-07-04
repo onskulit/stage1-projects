b806eb56f452b60c0093fed60fbb2f4cc43bda73
onskulit <dislaty@mail.com>
2022-07-04 15:20:11 +0300

    refactor: refactor index.ts according to eslint

--- migration-newip-to-ts/src/Interfaces-api/index.ts
+++ migration-newip-to-ts/src/Interfaces-api/index.ts
@@ -3,75 +3,62 @@ export type stringOrNull = string | null;
 export type HTMLElementOrNull = HTMLElement | null;
 
 interface INegativeResponse {
-  code?: string,
-  message?: string,
+    code?: string;
+    message?: string;
 }
 
 export interface IArticle {
-  source: {
-    id: stringOrNull,
-    name: stringOrNull,
-  },
-  author: stringOrNull,
-  title: stringOrNull,
-  description: stringOrNull,
-  url: string,
-  urlToImage: stringOrNull,
-  publishedAt: string,
-  content: stringOrNull
+    source: {
+        id: stringOrNull;
+        name: stringOrNull;
+    };
+    author: stringOrNull;
+    title: stringOrNull;
+    description: stringOrNull;
+    url: string;
+    urlToImage: stringOrNull;
+    publishedAt: string;
+    content: stringOrNull;
 }
 
 export interface IResponse extends INegativeResponse {
-  status: statusType,
-  totalResults?: number,
-  articles?: IArticle[],
-  sources?: ISource[],
+    status: statusType;
+    totalResults?: number;
+    articles?: IArticle[];
+    sources?: ISource[];
 }
 
-export interface INewsResponse extends INegativeResponse { 
-  status: statusType,
-  totalResults?: number,
-  articles?: IArticle[],
-};
+export interface INewsResponse extends INegativeResponse {
+    status: statusType;
+    totalResults?: number;
+    articles?: IArticle[];
+}
 
 export interface ISource {
-  id: string,
-  name: stringOrNull,
-  description: stringOrNull,
-  url: stringOrNull,
-  category: stringOrNull,
-  country: stringOrNull
+    id: string;
+    name: stringOrNull;
+    description: stringOrNull;
+    url: stringOrNull;
+    category: stringOrNull;
+    country: stringOrNull;
 }
 
-export interface ISourcesResponse extends INegativeResponse { 
-  status: statusType,
-  sources?: ISource[],
-};
+export interface ISourcesResponse extends INegativeResponse {
+    status: statusType;
+    sources?: ISource[];
+}
 
 export interface IOptions {
-  apiKey?: string;
-  q?: string;
-  searchIn?: string;
-  sources?: string;
-  domains?: string;
-  excludeDomains?: string;
-  from?: string;
-  to?: string;
-  language?: string;
-  sortBy?: string;
-  pageSize?: string;
-  page?: string;
+    apiKey?: string;
+    q?: string;
+    searchIn?: string;
+    sources?: string;
+    domains?: string;
+    excludeDomains?: string;
+    from?: string;
+    to?: string;
+    language?: string;
+    sortBy?: string;
+    pageSize?: string;
+    page?: string;
 }
-
-/* const Response: IResponse = {
-    status: 'error',
-    sources: [
-        {id: '2',
-        name: 'string',
-        description: 'string',
-        url: 'string',
-        category: 'string',
-        country: 'string'
-        },
-    ],
-} */
