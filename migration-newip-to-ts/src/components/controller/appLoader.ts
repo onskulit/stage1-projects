f58cbbd00633590aa5ce2f841953d0a5bd4495bc
onskulit <dislaty@mail.com>
2022-07-04 15:54:03 +0300

    fix: update link for API

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader, { ILoader } from './loader';
 
 class AppLoader extends Loader implements ILoader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '145a3d7de4f242a6ba17b7bc2e18f6f2',
         });
     }
